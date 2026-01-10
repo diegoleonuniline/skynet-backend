a83081523cb5117543c20744566138d5f7aff3b8
diegoleonuniline <desarrollos@uniline.mx>
2026-01-10 11:39:45 -0600

    Delete src directory

--- src/rutas/cargos.rutas.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { obtenerCargos, obtenerMensualidades, crearCargo } = require('../controladores/cargos.controlador');
-
-router.get('/', obtenerCargos);
-router.get('/mensualidades', obtenerMensualidades);
-router.post('/', crearCargo);
-
-module.exports = router;
--- src/rutas/dashboard.rutas.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { obtenerEstadisticas } = require('../controladores/dashboard.controlador');
-
-router.get('/estadisticas', obtenerEstadisticas);
-
-module.exports = router;
