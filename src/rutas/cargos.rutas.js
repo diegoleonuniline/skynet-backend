e434af712f87356bcb6751378a7ffc374da32322
diegoleonuniline <desarrollos@uniline.mx>
2026-01-10 08:52:52 -0600

    Update cargos.rutas.js

--- src/rutas/cargos.rutas.js
+++ src/rutas/cargos.rutas.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { obtenerCargos, crearCargo } = require('../controladores/cargos.controlador');
+const { obtenerCargos, obtenerMensualidades, crearCargo } = require('../controladores/cargos.controlador');
 
 router.get('/', obtenerCargos);
+router.get('/mensualidades', obtenerMensualidades);
 router.post('/', crearCargo);
 
 module.exports = router;
